0d2ce79748802e3071857356591bfc9f2e3850fc
Mykola Marchuk <mmarchuk@Mykolas-MacBook-Pro.local>
2025-07-06 08:15:39 +0300

    init logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,20 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 
 	"github.com/marchuknikolay/url-shortener/internal/config"
 )
 
+const (
+	local = "local"
+	dev   = "dev"
+	prod  = "prod"
+)
+
 func main() {
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
 
-	// init logger
+	log := initLogger(cfg.Env)
+
+	log.Info("Starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("Debug messages are enabled")
 
 	// init repository
 
 	// init router
 
 	// run server
+}
+
+func initLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case local:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case dev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case prod:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
